1deb1f37d76c0e0857de4215f9e9ebf7f27c59a7
Pedro Amorim Brandão da Silva <pedro.brandao.silva@gmail.com>
2012-04-10 12:20:28 +0100

    Some upgrades to the camera library.

--- SXL.Cameras/OrthographicCamera.cs
+++ SXL.Cameras/OrthographicCamera.cs
@@ -2,38 +2,45 @@
 
 namespace SXL.Cameras
 {
-    public class TargetCamera : Camera
+    public class OrthographicCamera : Camera
     {
-        private float _fieldOfViewDegrees = 60f;
+        private float _width;
+        private float _height;
         private float _nearPlaneDistance = 1f;
         private float _farPlaneDistance = 1000;
 
-        public TargetCamera(GraphicsDeviceManager graphics)
-            : base(graphics)
+        public OrthographicCamera(GraphicsDeviceManager newGraphics) 
+            : base(newGraphics)
         {
+            _width = graphics.GraphicsDevice.Viewport.Width;
+            _height = graphics.GraphicsDevice.Viewport.Height;
         }
 
         public override void Initialize()
         {
+            projectionMatrix = Matrix.CreateOrthographic(_width, _height, _nearPlaneDistance, _farPlaneDistance);
             viewMatrix = Matrix.CreateLookAt(position, target, UpVector);
-            projectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(60f), graphics.GraphicsDevice.Viewport.AspectRatio, _nearPlaneDistance, _farPlaneDistance);
         }
 
         public override void Update(GameTime gameTime)
         {
-            base.Update(gameTime);
-
             //sets up the view in case it was changed
             //if (bUpdateView)
-                viewMatrix = Matrix.CreateLookAt(Position, target, UpVector);
+            viewMatrix = Matrix.CreateLookAt(Position, target, UpVector);
         }
 
         #region Getters and Setters
 
-        public float FieldOfViewDegrees
+        public float Height
         {
-            get { return _fieldOfViewDegrees; }
-            set { _fieldOfViewDegrees = value; }
+            get { return _height; }
+            set { _height = value; }
+        }
+
+        public float Width
+        {
+            get { return _width; }
+            set { _width = value; }
         }
 
         public float FarPlaneDistance
