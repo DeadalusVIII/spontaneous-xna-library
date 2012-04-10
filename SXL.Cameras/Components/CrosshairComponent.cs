2fbd03c1f1701f2ff09d4f45059c93fb7cc11eea
Pedro Silva <Pedro Silva@localhost>
2012-04-10 12:20:28 +0100

    Some upgrades to the camera library.

--- SXL.Cameras/Components/CrosshairComponent.cs
+++ SXL.Cameras/Components/CrosshairComponent.cs
@@ -6,20 +6,26 @@ namespace SXL.Cameras.Components
 {
     public class CrosshairComponent : CameraComponent
     {
-        private Vector2 crosshairPosition;
-        private Texture2D crosshairTexture;
+        private readonly string _crosshairPath;
+        private Vector2 _crosshairPosition;
+        private Texture2D _crosshairTexture;
+
+        public CrosshairComponent(string crosshairPath)
+        {
+            _crosshairPath = crosshairPath;
+        }
 
         public override void LoadContent(ContentManager contentManager)
         {
-            crosshairTexture = contentManager.Load<Texture2D>("Cameras/Crosshairs/Crosshair01");
+            _crosshairTexture = contentManager.Load<Texture2D>(_crosshairPath);
 
-            crosshairPosition = new Vector2(Camera.Graphics.GraphicsDevice.Viewport.Width / 2.0f - crosshairTexture.Width / 2.0f,
-                                            Camera.Graphics.GraphicsDevice.Viewport.Height / 2.0f - crosshairTexture.Height / 2.0f);
+            _crosshairPosition = new Vector2(Camera.Graphics.GraphicsDevice.Viewport.Width / 2.0f - _crosshairTexture.Width / 2.0f,
+                                            Camera.Graphics.GraphicsDevice.Viewport.Height / 2.0f - _crosshairTexture.Height / 2.0f);
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(crosshairTexture, crosshairPosition, Color.White);
+            spriteBatch.Draw(_crosshairTexture, _crosshairPosition, Color.White);
         }
     }
 }
